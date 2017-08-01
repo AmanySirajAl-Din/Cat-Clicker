bdc129bb5957e69bd8b02181e4bdc2b662590a21
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-01 15:30:48 +0200

    Add cats to the side list dynimacally

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
 // Add cat names Dynamicaly
-var cat_names = ["Bosy", "Misho"];
+var catNames = ["Bosy", "Misho", "Mared", "Bisho", "Noor", "Boby"];
+var CatItem;
 
-$(".cat-name").each(function(i){
-    $(this).text(cat_names[i]);
-});
+// add cats to the side list dynimacally
+for(var i=0; i<catNames.length; i++){
+    CatItem = '<div class="cat-list-container cat-Btn cat-up">';
+    CatItem += '<h4 class="cat-name-list">' + catNames[i] + '</h4>';
+    CatItem += '<img src="img/' + catNames[i] + '.jpg" />';
+    CatItem += '</div>';
+    $(".cats-list").append(CatItem);
+}
 
 // represent the numer of clicks
-var click_num = Number($(".click-num").text());
-$(".cat-img").click(function(){
-    click_num++;
-    $(".click-num").text(click_num);
+var clickNum = Number($(".click-num").text());
+$(".cat-img").click(function () {
+    clickNum++;
+    $(".click-num").text(clickNum);
 });
-$(".cat-Btn").mousedown(function(){
-    $(this).css({
-        "top": "5px",
-        "left": "4px"
+$(".cat-Btn").mousedown(function () {
+        $(this).css({
+            "top": "5px",
+            "left": "4px"
+        });
+        $(this).removeClass("cat-up");
+    })
+    .mouseup(function () {
+        $(this).css({
+            "top": "0",
+            "left": "0"
+        });
+        $(this).addClass("cat-up");
     });
-    $(this).removeClass("cat-up");
-})
-.mouseup(function(){
-    $(this).css({
-        "top": "0",
-        "left": "0"
-    });
-    $(this).addClass("cat-up");
-});
-
