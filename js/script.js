b94a006ffb0fce65e1a674afab69154081ca43e7
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-01 13:27:38 +0200

    Add click effect to cat image

--- js/script.js
+++ js/script.js
@@ -2,4 +2,18 @@ var click_num = Number($(".click-num").text());
 $(".cat-img").click(function(){
     click_num++;
     $(".click-num").text(click_num);
-});
+})
+.mousedown(function(){
+    $(this).css({
+        "top": "5px",
+        "left": "4px"
+    });
+    $(this).removeClass("cat-up");
+})
+.mouseup(function(){
+    $(this).css({
+        "top": "0",
+        "left": "0"
+    });
+    $(this).addClass("cat-up");
+})
