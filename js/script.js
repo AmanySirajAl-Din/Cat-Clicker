147c09eb89d806dc073673a1b57311a03da6fd15
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-02 18:23:11 +0200

    Intialize the catView variable

--- js/script.js
+++ js/script.js
@@ -2,26 +2,31 @@
 
 var model = {
     activeCat: null,
-    catsObjArray:[
+    catsObjArray: [
         {
-            numOfClicks : 0,
-            name : "Bisho"
+            numOfClicks: 0,
+            name: "Bisho",
+            imgSrc: "img/Bisho.jpg"
         },
         {
-            numOfClicks : 0,
-            name : "Boby"
+            numOfClicks: 0,
+            name: "Boby",
+            imgSrc: "img/Boby.jpg"
         },
         {
-            numOfClicks : 0,
-            name : "Bosy"
+            numOfClicks: 0,
+            name: "Bosy",
+            imgSrc: "img/Bosy.jpg"
         },
         {
-            numOfClicks : 0,
-            name : "Mared"
+            numOfClicks: 0,
+            name: "Mared",
+            imgSrc: "img/Mared.jpg"
         },
         {
-            numOfClicks : 0,
-            name : "Misho"
+            numOfClicks: 0,
+            name: "Misho",
+            imgSrc: "img/Misho.jpg"
         }
     ]
 };
@@ -30,36 +35,61 @@ var model = {
 /* ======= Octopus ======= */
 
 var octopus = {
-    init: function(){
+    init: function () {
         // put the first cat on model.activeCat Obj
         // to be showen in my page
         model.activeCat = model.catsObjArray[0];
-        
+
         // Here will be the View init
-        
+
     },
-    
+
     // get the data from the model
-    getActiveCat: function(){
+    getActiveCat: function () {
         return model.activeCat;
     },
-    
-    getCatsObjArray: function(){
+
+    getCatsObjArray: function () {
         return model.catsObjArray;
     },
-    
-    
+
+
 };
 
 
 /* ======= View ======= */
 
-var catView = {
-    
+var catView = { // for the active cat section
+    init: function () { // run once
+
+        // click event for the cat image
+        $(".active-cat-img").on("click", function () {
+            // increase cat's numOfClicks (in Model) 
+            // then use Octopus fun
+
+        });
+        
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function () {
+        var imgSrcPart1 = "",
+        // update the DOM elements with values from the current cat
+        // first, get the active cat to get its values
+        var activeCat = octopus.getActiveCat();
+        // update the the cat name header to the activeCat name
+        $(".cat-name").text(activeCat.name);
+        // update the active cat photo by updating its src attribute
+        $(".active-cat-img").attr("src", activeCat.imgSrc);
+        // finally update the showen number of clicks for the active cat
+        $(".click-num").text(activeCat.numOfClicks);
+    }
+
 };
 
 var catListVeiw = {
-    
+
 };
 
 /*
